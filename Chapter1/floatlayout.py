116062eb406ebe81c3ec2358b921c1247f41f86f
DavidsIT-Site <44936577+DavidsIT-Site@users.noreply.github.com>
2019-01-03 19:52:18 -0500

    float layout

---
+++ Chapter1/floatlayout.py
@@ -0,0 +1,17 @@
+import kivy
+kivy.require('1.9.0')
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+
+
+class FloatLayoutApp(App):
+	def build(self):
+			return FloatLayout()
+
+if __name__ == '__main__':
+	FloatLayoutApp().run()
+
+
+class WidgetsApp(App):
+	def build(self):
+		return MyWidget()
